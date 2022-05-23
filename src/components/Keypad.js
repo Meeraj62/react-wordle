e801b4b0736a0cf5208b6d8945f0f3aeda15f4b1
Meeraj Adhikari <atifmeeraj62@gmail.com>
2022-05-23 12:01:38 +0545

    Designed Keypad letters in the wordle component

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -6,7 +6,7 @@ export default function Keypad() {
     const [letters, setLetters] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:30001/letters')
+        fetch('http://localhost:3001/letters')
             .then(response => response.json())
             .then(data => {
                 setLetters(data);
@@ -17,9 +17,9 @@ export default function Keypad() {
         <div className='keypad'>
             {letters && letters.map((letter) => {
                 return (
-                    <button key={letter.id}>
-                        {letter.letter}
-                    </button>
+                    <div key={letter.key}>
+                        {letter.key}
+                    </div>
                 )
             })}
         </div>
