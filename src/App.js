643c8fb6f8869b96a0841f9d6e0db1410e4e21a7
Meeraj Adhikari <atifmeeraj62@gmail.com>
2022-05-24 18:27:22 +0545

    Added missing css file for toaster notification

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import Wordle from './components/Wordle'
 
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [solution, setSolution] = useState(null)
